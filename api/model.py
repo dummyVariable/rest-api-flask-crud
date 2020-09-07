986c781591e9696b02414a7ca59f5b401349349c
dummyVariable <officialsanjivee@gmail.com>
2020-09-08 00:20:16 +0530

    created model

--- api/model.py
+++ api/model.py
@@ -1,5 +1,36 @@
+from typing import TypeVar
 
-data = []
+
+
+data = {}
 
 class CrudModel:
-    pass
+    counter = 0
+
+    @staticmethod
+    def create(item: dict) -> str:
+        if not isinstance(item, dict):
+            return None
+        CrudModel.counter += 1
+        data[CrudModel.counter] = item
+        return 'ok'
+    
+    @staticmethod
+    def read(id: int) -> dict:
+        return data.get(id)
+
+    @staticmethod
+    def update(id: int, item: dict) -> str:
+        if not isinstance(item, dict):
+            return None  
+        if id not in data.keys():
+            return None
+        data[id] = item
+        return 'ok'
+
+    @staticmethod
+    def delete(id: int) -> str:
+        if id not in data.keys():
+            return None
+        del data[id]
+        return 'ok'
